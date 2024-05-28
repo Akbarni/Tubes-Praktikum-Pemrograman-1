224678edc1bd0f49197f8897e7d50160c398d52d
Akbar Nur iman <122083444+Akbarni@users.noreply.github.com>
2024-05-28 22:40:59 +0700

    Mengumpulkan Tugas Besar Praktikum Pemrograman 1

---
+++ TubesPrakProm/Main.java
@@ -0,0 +1,8 @@
+package TubesPrakProm;
+
+public class Main {
+    public static void main(String[] args) {
+        BankingGraph bankingGraphApp = new BankingGraph();
+        bankingGraphApp.start();
+    }
+}
